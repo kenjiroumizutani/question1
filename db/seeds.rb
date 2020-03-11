a8730b221d1adf0b1323afad6db2786fe14d626f
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-11 10:36:53 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+questions_csv = CSV.readlines("db/questions.csv")
+questions_csv.each do |row|
+  Question.create(questions: row[1], choice1: row[2], choice2: row[5], choice3: row[6], answer: row[7], created_at: row[8], update_at: row[9], title: row[10], correct: row[11], comment:[12])
+  # Product.createのproductは変えなくていいの？変える？
+end

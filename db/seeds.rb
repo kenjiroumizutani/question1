e5844b2c9a3bb30c9ae118bf6789b2c2a26391dc
EC2 Default User <ec2-user@ip-172-31-45-49.ap-northeast-1.compute.internal>
2020-03-11 10:59:18 +0000

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,5 @@ require "csv"
 
 questions_csv = CSV.readlines("db/questions.csv")
 questions_csv.each do |row|
-  Question.create(questions: row[1], choice1: row[2], choice2: row[5], choice3: row[6], answer: row[7], created_at: row[8], update_at: row[9], title: row[10], correct: row[11], comment:[12])
-  # Product.createのproductは変えなくていいの？変える？
+  Question.create(questions: row[1], choice1: row[2], choice2: row[3], choice3: row[4], answer: row[5], created_at: row[6], update_at: row[7], title: row[8], correct: row[9], comment:[10])
 end
